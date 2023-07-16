c60f4506d0a92c6d80b85a3c0f7664505bf862af
Benjamin Harry Skoler <bhs240@nyu.edu>
2023-07-16 16:31:35 +0300

    added remaining time notifications v.12

--- spotify/sleep.py
+++ spotify/sleep.py
@@ -184,7 +184,7 @@ def start_timer(update, context):
     remaining_time = sleep_duration - (time.time() - start_time)
     logging.warning(f"remaing time is {remaining_time}")
 
-    while remaining_time - start_time > 60:
+    while remaining_time > 60:
         # Caluclate time remaining
         minutes_remaining = (remaining_time // 60)
 
